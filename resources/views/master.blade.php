c91a3b40305a94d1dfbe60f80791bae65678d090
Tony Starikov <tony.starikov.1992@gmail.com>
2023-07-23 19:47:10 +0300

    Added google optimize script

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,10 @@
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T276DWG');</script>
     <!-- End Google Tag Manager -->
 
+    <!-- Google Optimize -->
+    <script src="https://www.googleoptimize.com/optimize.js?id=OPT-WL7D2N2"></script>
+    <!-- End Google Optimize -->
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
