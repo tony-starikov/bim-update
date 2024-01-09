ce26e5f11d8d109867e591abffbff9f4551d5da9
Tony Starikov <tony.starikov.1992@gmail.com>
2024-01-10 00:55:02 +0200

    Edited portfolio on the main page

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -357,51 +357,29 @@
                 </div>
             </div>
 
-            <div class="row justify-content-evenly p-2 px-sm-5">
+            <div class="row w-100 justify-content-evenly p-2 px-sm-5">
                 <div class="col-12 px-lg-5">
                     <div class="row px-lg-5">
                         @foreach($works as $work)
                             <div class="col-md-6 col-xl-4 mt-4">
-                                <div class="portfolio-container">
-                                    <div class="card border-0">
-                                        <a class="text-decoration-none" href="{{ route('portfolio.project', $work->slug) }}">
-                                            <img src="/images/{{ $work->image }}" class="card-img" alt="{{ $work->title }}">
-                                        </a>
-                                        <div class="d-lg-none">
-                                            <div class="row align-items-center">
-                                                <div class="col-7 mt-2">
-                                                    <a class="text-decoration-none" href="{{ route('portfolio.project', $work->slug) }}">
-                                                        <h3 class="text-dark h5 fw-bold m-0">{{ $work->title }}</h3>
-                                                    </a>
-                                                </div>
-                                                <div class="col-5 mt-2">
-                                                    <a href="{{ route('portfolio.project', $work->slug) }}" class="text-decoration-none">
-                                                        <button class="btn btn-primary btn-lg border-0 shadow-none rounded-4 w-100" style="background-color: #43aeb6" type="button">
-                                                            <span class="fw-bold h6 d-block my-1">EXPLORE</span>
-                                                        </button>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="overlay d-none d-lg-block">
-                                        <div class="row h-100 justify-content-center align-items-center text-center text-white">
-                                            <div class="col-12 mt-2 p-4">
+                                <div class="card border-0">
+                                    <a class="text-decoration-none" href="{{ route('portfolio.project', $work->slug) }}">
+                                        <img src="/images/{{ $work->image }}" class="card-img" alt="{{ $work->title }}">
+                                        <div class="row align-items-center">
+                                            <div class="col-7 mt-2">
                                                 <a class="text-decoration-none" href="{{ route('portfolio.project', $work->slug) }}">
-                                                    <h3 class="text-white h5 fw-bold m-0">{{ $work->title }}</h3>
+                                                    <h3 class="text-dark h6 fw-bold m-0">{{ $work->title }}</h3>
                                                 </a>
-                                                <div class="row justify-content-center p-4">
-                                                    <div class="col-8">
-                                                        <a href="{{ route('portfolio.project', $work->slug) }}" class="text-decoration-none">
-                                                            <button class="btn btn-primary btn-lg border-0 shadow-none rounded-4 w-100" style="background-color: #43aeb6" type="button">
-                                                                <span class="fw-bold h5 d-block my-2">EXPLORE</span>
-                                                            </button>
-                                                        </a>
-                                                    </div>
-                                                </div>
+                                            </div>
+                                            <div class="col-5 mt-2">
+                                                <a href="{{ route('portfolio.project', $work->slug) }}" class="text-decoration-none">
+                                                    <button class="btn btn-primary btn-lg border-0 shadow-none rounded-4 w-100" style="background-color: #43aeb6" type="button">
+                                                        <span class="fw-bold h6 d-block my-1">EXPLORE</span>
+                                                    </button>
+                                                </a>
                                             </div>
                                         </div>
-                                    </div>
+                                    </a>
                                 </div>
                             </div>
                         @endforeach
